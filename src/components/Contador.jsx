99036f633281974fbc686aa388d7e410843f2770
Joaquin Gonzalez  Menza <holaj3415@gmail.com>
2023-09-07 23:15:56 -0300

    1era-Prentrega-Gonzalez

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,19 +1,22 @@
 import { useState } from "react";
 
-
 const Contador = () => {
-    // LOGICA
-    const [numero, setNumero] = useState(1);
-    const handCount = () => setNumero(numero + 1);
-    //LOGICA
+// LOGICA
+
+const [numero, setNumero] = useState(1);
+const handCount = () => setNumero(numero + 1);
+
+//LOGICA
+
 return (
     //HTML
     <div>
-    {numero}
-    <hr />
-    <button onClick={handCount}>Sumar</button>
+        {numero}
+        <hr />
+        <button onClick={handCount}>Sumar</button>
     </div>
     //HTML
-);};
+);
+};
 
 export default Contador;
